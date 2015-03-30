670fb1768f109fb9027607c849a2c1a0fb48c148
malinger <475968396@qq.com>
2015-03-30 16:31:03 +0800

    完成了SVG到png的转换，更改了IMAGE

---
+++ NBA_Stat_Sys/src/ui/SVG2PNG.java
@@ -0,0 +1,48 @@
+package ui;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.batik.transcoder.Transcoder;
+import org.apache.batik.transcoder.TranscoderException;
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.image.ImageTranscoder;
+import org.apache.batik.transcoder.image.PNGTranscoder;
+
+public class SVG2PNG {
+	public static void convertSvgFileToPng(File svg, File png)
+			throws IOException, TranscoderException {
+		InputStream in = new FileInputStream(svg);
+		OutputStream out = new FileOutputStream(png);
+		out = new BufferedOutputStream(out);
+		convertToPNG(in, out);
+	}
+
+	private static void convertToPNG(InputStream in, OutputStream out)
+			throws IOException, TranscoderException {
+		Transcoder transcoder = new PNGTranscoder();
+		try {
+			TranscoderInput input = new TranscoderInput(in);
+			try {
+				TranscoderOutput output = new TranscoderOutput(out);
+				transcoder.addTranscodingHint(ImageTranscoder.KEY_WIDTH,
+						Float.parseFloat(String.valueOf(500)));// 设置图片大小
+				transcoder.transcode(input, output);
+
+			} finally {
+				out.close();
+			}
+		} finally {
+			in.close();
+		}
+	}
+	public static void main(String[] args) throws IOException, TranscoderException{
+		convertSvgFileToPng(new File("CSEdata/teams/ATL.svg"),new File("E:/a.png"));
+	}
+}
