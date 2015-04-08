e6824860fe1a16e4041c0c0d4443f6aad15d9d23
InnoMachine <jx13@software.nju.edu.cn>
2015-04-08 15:06:18 +0800

    correct the maven project

---
+++ NBA_Stat_Sys2/src/dataService/Test.java
@@ -0,0 +1,10 @@
+package dataService;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a maven project test!");
+	}
+
+}
