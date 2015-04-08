13ca64806789c01f7f1b2b03b375ddf7fecf57c7
王嘉祺 <wjq13@software.nju.edu.cn>
2015-04-08 20:21:38 +0800

    new

---
+++ NBA_Stat_Sys/src/vo/TeamRecentGames.java
@@ -0,0 +1,47 @@
+package vo;
+
+import java.util.ArrayList;
+
+public class TeamRecentGames {
+	private String name;
+	private String abbreviation;
+	private ArrayList<TeamPerformanceInSingleGame> fiveGames;
+	
+	public void AddNewGame(TeamPerformanceInSingleGame ngame){
+		if(fiveGames.size()==5)
+		{
+			fiveGames.remove(0);
+			fiveGames.add(ngame);
+		}
+		else if(fiveGames.size()>5)
+		{
+			while(fiveGames.size()>=5)
+			{
+				fiveGames.remove(0);
+			}
+			fiveGames.add(ngame);
+		}
+		else
+			fiveGames.add(ngame);
+	}
+	public ArrayList<TeamPerformanceInSingleGame> getFiveGames()
+	{
+		return fiveGames;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAbbreviation() {
+		return abbreviation;
+	}
+
+	public void setAbbreviation(String abbreviation) {
+		this.abbreviation = abbreviation;
+	}
+}
