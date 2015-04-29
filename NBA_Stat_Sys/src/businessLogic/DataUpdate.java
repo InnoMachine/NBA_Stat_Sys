00f953d42dfcd1918309c24e6cd4dfc0a06e307c
王嘉祺 <wjq13@software.nju.edu.cn>
2015-04-29 14:23:52 +0800

    sm

--- NBA_Stat_Sys/src/businessLogic/DataUpdate.java
+++ NBA_Stat_Sys/src/businessLogic/DataUpdate.java
@@ -3,14 +3,17 @@ package businessLogic;
 import java.util.TimerTask;
 
 import po.GameDate;
+import ui.MainFrame;
 
 public class DataUpdate extends TimerTask{
 	GameDate dateui;
 	GameDate dateTrue;
 	Data_Handler data_handler ;
+	MainFrame mf;
 	
 	public DataUpdate(){
 		dateTrue = new GameDate();
+		mf = new MainFrame();
 		data_handler = Data_Handler.getInstance();
 		dateui = data_handler.getDateNow();
 	}
