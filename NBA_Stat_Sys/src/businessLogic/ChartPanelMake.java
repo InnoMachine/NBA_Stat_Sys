03ad6d0fac74ca02b5927e0b9385c230e051aa10
王嘉祺 <wjq13@software.nju.edu.cn>
2015-06-17 01:31:20 +0800

    gai

--- NBA_Stat_Sys/src/businessLogic/ChartPanelMake.java
+++ NBA_Stat_Sys/src/businessLogic/ChartPanelMake.java
@@ -22,7 +22,7 @@ public class ChartPanelMake {
 		return lcm.makeChart(dataset);
 	}
 	
-	public ChartPanel getScatterChartPanel(int number,String abbr,String season,int n){
+	public ChartPanel getScatterChartPanel(int number,String abbr,String season,String n){
 		ScatterPlotChartMaking spcm = new ScatterPlotChartMaking();
 		DefaultXYDataset dataset = new DefaultXYDataset();
 		//dataset = dataset1();
